ca1dd1470648180817cca25cf76f932bdc3dc29f
Matteo Sabaudo Rodrigues <116103162+MatteoSabaudoRodrigues@users.noreply.github.com>
2022-10-30 13:51:51 +0100

    Update Currency-Converter.py

--- Currency-Converter.py
+++ Currency-Converter.py
@@ -6,6 +6,4 @@ amount = float(input("Amount: "))
 
 response = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
 
-results_to = {response.json()['rates'][to_currency]}
-
-print(f"${amount} {from_currency} is ${results_to} {to_currency}")
+print(f"${amount} {from_currency} is {response.json()['rates'][to_currency]} {to_currency}")
